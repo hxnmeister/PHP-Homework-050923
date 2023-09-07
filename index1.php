d9893d7ed4372f194766b407d601a224621b6c40
Valerii <valeriy.stronskiy@gmail.com>
2023-09-07 14:37:22 +0300

    Finished task 1

---
+++ index1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Homework</title>
+        </head>
+        <body>
+            <?php 
+                $endNumber = 7;
+                $sum = 0;
+
+                for($i = 2; $i <= $endNumber; $i += 2):
+
+                    $sum += $i;
+            ?>
+                <span style="font-size:<?php echo $i * 5?>px; color:green"><?php echo $i.($i + 2 > $endNumber ? ' ' : ',')?></span>
+            <?php 
+                endfor;
+                
+                echo '<p>Sum = '.$sum.'</p>';
+                echo '<p>AVG = '.$sum / (($endNumber % 2 === 0 ? $endNumber : $endNumber - 1) / 2).'</p>';
+            ?>
+        </body>
+    </html>
